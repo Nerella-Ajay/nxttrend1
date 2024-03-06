8373454a31c52be788699c44e81873118f077a07
ajay <nerellaajay98@gmail.com>
2024-03-06 06:48:46 +0000

    solution

---
+++ src/main/java/com/example/nxttrendz1/service/ProductJpaService.java
@@ -0,0 +1,82 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.nxttrendz1.service;
+
+import com.example.nxttrendz1.repository.ProductJpaRepository;
+import com.example.nxttrendz1.repository.ProductRepository;
+import com.example.nxttrendz1.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductJpaService implements ProductRepository {
+    @Autowired
+    private ProductJpaRepository productJpaRepositroy;
+
+    @Override
+    public ArrayList<Product> getProducts() {
+        List<Product> productList = productJpaRepositroy.findAll();
+        ArrayList<Product> products = new ArrayList<>(productList);
+        return products;
+    }
+
+    @Override
+    public Product getProductById(int productId) {
+        try {
+            Product product = productJpaRepositroy.findById(productId).get();
+            return product;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        productJpaRepositroy.save(product);
+        return product;
+    }
+
+    @Override
+    public Product updateProduct(int productId, Product product) {
+        try {
+            Product newProduct = productJpaRepositroy.findById(productId).get();
+            if (product.getProductName() != null) {
+                newProduct.setProductName(product.getProductName());
+            }
+            if (product.getPrice() != 0) {
+                newProduct.setPrice(product.getPrice());
+            }
+            productJpaRepositroy.save(newProduct);
+            return newProduct;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+        try {
+            productJpaRepositroy.deleteById(productId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/example/nxttrendz1/service/ReviewJpaService.java
@@ -0,0 +1,112 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.nxttrendz1.service;
+
+import com.example.nxttrendz1.repository.*;
+import com.example.nxttrendz1.model.Review;
+import com.example.nxttrendz1.model.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReviewJpaService implements ReviewRepository {
+    @Autowired
+    private ReviewJpaRepository reviewJpaRepository;
+
+    @Autowired
+    private ProductJpaRepository ProductJpaRepositroy;
+
+    @Override
+    public ArrayList<Review> getReviews() {
+        List<Review> reviewsList = reviewJpaRepository.findAll();
+        ArrayList<Review> reviews = new ArrayList<>(reviewsList);
+        return reviews;
+    }
+
+    @Override
+    public Review getReviewById(int reviewId) {
+        try {
+            Review review = reviewJpaRepository.findById(reviewId).get();
+            return review;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Review addReview(Review review) {
+        Product product = review.getProduct();
+        int productId = product.getProductId();
+
+        try {
+            product = ProductJpaRepositroy.findById(productId).get();
+            review.setProduct(product);
+            reviewJpaRepository.save(review);
+            return review;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Review updateReview(int reviewId, Review review) {
+        try {
+            Review newReview = reviewJpaRepository.findById(reviewId).get();
+            if (review.getProduct() != null) {
+                int productId = review.getProduct().getProductId();
+                Product newProduct = ProductJpaRepositroy.findById(productId).get();
+                newReview.setProduct(newProduct);
+            }
+            if (review.getReviewContent() != null) {
+                newReview.setReviewContent(review.getReviewContent());
+            }
+            if (review.getRating() != 0) {
+                newReview.setRating(review.getRating());
+            }
+            reviewJpaRepository.save(newReview);
+            return newReview;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteReview(int reviewId) {
+        try {
+            reviewJpaRepository.deleteById(reviewId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public Product getReviewProduct(int reviewId) {
+        try {
+            Review review = reviewJpaRepository.findById(reviewId).get();
+            Product product = review.getProduct();
+            return product;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
